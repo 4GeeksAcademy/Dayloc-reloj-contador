860bdcf34cfcc12b44d2544a3ce7bc681b43aa92
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-14 00:04:48 +0000

    falta los minutos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import Counter from "./SecoundCounter";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+ let Contador=0;
+ function Incremento(contador){
+	contador++;
+	console.log(contador)
+	
+	 
+}
+
+
+
+let seg = setInterval(Incremento,1000);
+
+console.log(seg)
+let segundos=seg;
+let minutos=0;
+let horas=0;
+
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+				<Counter segundos={segundos} minutos={minutos} horas={horas} />
+		</div>	
+				
 	);
 };
 
