3b011fbbba7927afa9a9e76c69bdc3b946f18588
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-16 21:31:48 +0000

    faltan los minutos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Counter from "./SecoundCounter";
-import {useState} from 'react';
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,29 +8,42 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 
 const Home = () => {
-  const[segundos,setSegundos] = useState(0);
-  let minutos = 0;
+  const [segundos, setSegundos] = useState(0);
+  const[minutos,setMinutos]  = useState(0);
   let horas = 0;
-  const incremento=()=>{
+ 
+  
+  useEffect(() => {
+    setInterval(() => {
+      
+      setSegundos((preSegundos)=>preSegundos===59 ? 0 : preSegundos+1);
+     
+      
+    }, 1000);
     
-    setSegundos(segundos+1)
-    if(segundos==10){
-      segundos=0;
-    }else segundos+1;
 
-    console.log(segundos)
+  }, []);
+ 
+  useEffect(() => {
+    setInterval(() => {
+      
+      setMinutos((preMinutos)=> preMinutos===59 ? 0 : preMinutos+1 );
+      console.log(minutos)
+      
+    }, 60000);
     
+
+  }, [
     
-    
-  }
-  //setInterval(incremento, 1000);
+  ]);
+ 
 
   return (
     <div className="container">
       <Counter segundos={segundos} minutos={minutos} horas={horas} />
-      
     </div>
   );
+
 };
 
 export default Home;
