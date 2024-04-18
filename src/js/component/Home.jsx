bd9a47dde201fd7eacedb23dfa8950b34ae6f74c
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-18 19:17:54 +0000

    Formato al documento

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -2,59 +2,43 @@ import React, { useEffect } from "react";
 import Counter from "./SecoundCounter";
 import { useState } from "react";
 
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 
 const Home = () => {
-  const [segundos, setSegundos] = useState(56);
-  const[minutos,setMinutos]  = useState(59);
-  const[horas,setHoras]  = useState(0);
- 
-  
+  const [segundos, setSegundos] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [horas, setHoras] = useState(0);
+
   useEffect(() => {
     setInterval(() => {
-      
-      setSegundos((preSegundos)=> preSegundos>59 ? 0 : preSegundos+1);
-           
+      setSegundos((preSegundos) => (preSegundos > 59 ? 0 : preSegundos + 1));
     }, 1000);
-    
-    
-
   }, []);
- 
-  
+
   useEffect(() => {
-  
-      
-    setMinutos((minutos) => segundos>59 ? minutos+1 : minutos && minutos>59 ? minutos=0 : minutos)
-      
-  
+    setMinutos((minutos) =>
+      segundos > 59
+        ? minutos + 1
+        : minutos && minutos > 59
+        ? (minutos = 0)
+        : minutos
+    );
+  }, [segundos]);
 
-    
-
-},[segundos]);
-useEffect(() => {
-  
-      
-  setHoras((horas) => minutos>59 ? horas+1 : horas && horas>24 ? horas=0 : horas)
-
-
-  
-
-},[minutos]);
-
-
+  useEffect(() => {
+    setHoras((horas) =>
+      minutos > 59 ? horas + 1 : horas && horas > 24 ? (horas = 0) : horas
+    );
+  }, [minutos]);
 
   return (
     <div className="container">
       <Counter segundos={segundos} minutos={minutos} horas={horas} />
-      
     </div>
   );
-
 };
 
 export default Home;
