46e875a9722b20f0f8489238fd595d3f259b8eb7
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-18 17:40:33 +0000

    Correccion de validacion

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -9,15 +9,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 
 const Home = () => {
-  const [segundos, setSegundos] = useState(0);
-  const[minutos,setMinutos]  = useState(0);
+  const [segundos, setSegundos] = useState(56);
+  const[minutos,setMinutos]  = useState(59);
   const[horas,setHoras]  = useState(0);
  
   
   useEffect(() => {
     setInterval(() => {
       
-      setSegundos((preSegundos)=> preSegundos===60 ? 0 : preSegundos+1);
+      setSegundos((preSegundos)=> preSegundos>59 ? 0 : preSegundos+1);
            
     }, 1000);
     
@@ -29,7 +29,7 @@ const Home = () => {
   useEffect(() => {
   
       
-    setMinutos((minutos) => segundos===60 ? minutos+1 : minutos && minutos>59 ? minutos=0 : minutos)
+    setMinutos((minutos) => segundos>59 ? minutos+1 : minutos && minutos>59 ? minutos=0 : minutos)
       
   
 
@@ -39,7 +39,7 @@ const Home = () => {
 useEffect(() => {
   
       
-  setHoras((horas) => minutos===60 ? horas+1 : horas && horas>=24 ? horas=0 : horas)
+  setHoras((horas) => minutos>59 ? horas+1 : horas && horas>24 ? horas=0 : horas)
 
 
   
